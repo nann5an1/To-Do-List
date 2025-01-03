48b49fb624b5a2c1bb3eb7a615c2d061e7d365b6
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2025-01-03 15:08:30 +0800

    modified to-do-list 3.08pm

--- script.js
+++ script.js
@@ -2,26 +2,32 @@ const addBtn = document.getElementById("add-btn");
 const delBtn = document.getElementById("del-btn");
 const input = document.querySelector("#input-text");
 const parent = document.getElementById("output-div");
-let taskCount = 0;
 let array = [];
+let selectedChild = null;
 
-
-// function testFunc(){
-//     alert("clicked");
-// }
 function addToList(textVal){
-    array.push(textVal);
     let child = document.createElement("li");
     child.style.color = "rgb(0, 19, 59)";
     child.style.fontSize = "1.7rem";
     child.style.fontVariant = "small-caps";
     child.textContent = textVal; //either .innerText or .textContent
     child.style.backgroundColor = "transparent";
+    
+    array.push(textVal);
     parent.appendChild(child);
+    child.addEventListener('click', () => { //select on the task
+        selectedChild = child;
+        child.style.color = "red";
+    });
 }
 
 function delFromList(){
-    alert("select the item first");
+    if(selectedChild){
+        let arrayIndex = array.indexOf(selectedChild.textContent);
+        let removedNode = parent.removeChild(selectedChild);
+        array.splice(arrayIndex, 1);
+        selectedChild = null;
+    }
 }
 
 function handleKeyEnter(event){
@@ -34,5 +40,3 @@ function handleKeyEnter(event){
 document.addEventListener('keydown', () => handleKeyEnter(event));
 addBtn.addEventListener('click', () => addToList(input.value));
 delBtn.addEventListener('click', () => delFromList());
-
-
